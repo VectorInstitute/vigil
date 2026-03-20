2247b6620b556c6f295aa2d203ace30f511b43b5
Amrit Krishnan <amrit110@gmail.com>
2026-03-20 10:36:55 -0400

    bpf: add bpf_endian.h include to fix bpf_ntohs undeclared error

--- bpf/lsm.c
+++ bpf/lsm.c
@@ -6,6 +6,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_endian.h>
 #include "headers/common.h"
 
 char __license[] SEC("license") = "GPL";
